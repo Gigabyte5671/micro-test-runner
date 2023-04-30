d93b0590132f0a479d35a7a98e7276e3656e1c3b
Giga <gigabyte5671@gmail.com>
2023-04-30 18:03:26 +1200

    Add support for testing asynchronous candidates
    Candidate functions can now be run asynchronously by chaining the test with the `.async()` modifier method.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-class MicroTestRunner {
+class MicroTestRunner <Async extends boolean> {
 	private candidate: Function;
+	private asynchronous = false as Async;
 	private args = [] as unknown[][];
 	private conditions = [] as unknown[];
 	private runs = 1;
@@ -15,17 +16,23 @@ class MicroTestRunner {
 	 * @param candidate The expression/function to test.
 	 * @returns The test-runner.
 	 */
-	static test (candidate: Function): MicroTestRunner {
-		const newTestRunner = new MicroTestRunner(candidate);
-		return newTestRunner;
+	static test (candidate: Function): MicroTestRunner<false> {
+		return new MicroTestRunner<false>(candidate);
+	}
+
+	/**
+	 * Run each test asynchronously.
+	 */
+	async (): MicroTestRunner<true> {
+		this.asynchronous = true as Async;
+		return this as MicroTestRunner<true>;
 	}
 
 	/**
 	 * Run each test multiple times.
 	 * @param number The number of times to run each test.
-	 * @returns 
 	 */
-	times (number: number): MicroTestRunner {
+	times (number: number): MicroTestRunner<Async> {
 		this.runs = number > 0 ? Math.ceil(number) : 1;
 		return this;
 	}
@@ -34,7 +41,7 @@ class MicroTestRunner {
 	 * Provide arguments to the candidate.
 	 * @param args 
 	 */
-	with (args: Array<unknown>): MicroTestRunner {
+	with (args: Array<unknown>): MicroTestRunner<Async> {
 		this.args.push(args);
 		return this;
 	}
@@ -42,38 +49,46 @@ class MicroTestRunner {
 	/**
 	 * The results to expect from each test.
 	 * @param conditions An array of expected results.
-	 * @returns 
+	 * @returns `true` if all test runs passed, `false` if any run failed.
 	 */
-	expect (conditions: Array<unknown>): boolean {
-		if (Array.isArray(conditions)) {
-			this.conditions = conditions;
-		} else {
-			this.conditions.push(conditions);
-		}
-		if (this.args.length <= 0) {
-			this.args.push([]);
-		}
-		for (const [index, argumentGroup] of this.args.entries()) {
-			this.currentRun = 0;
-			while (this.currentRun < this.runs) {
-				let result = undefined;
-				try {
-					result = this.candidate.apply(undefined, argumentGroup) as unknown;
-				} catch (error) {
-					console.warn('MicroTestRunner: Test failed.', error);
-				}
-				const condition = this.conditions[index];
-				if (typeof condition === 'function') {
-					this.passing.push(condition(result));
-				} else {
-					this.passing.push(result === this.conditions[index]);
-				}
-				this.currentRun++;
+	expect (conditions: Array<unknown>): Async extends true ? Promise<boolean> : boolean {
+		const promise = new Promise<boolean>( async (resolve) => {
+			if (Array.isArray(conditions)) {
+				this.conditions = conditions;
+			} else {
+				this.conditions.push(conditions);
 			}
-		}
+			if (this.args.length <= 0) {
+				this.args.push([]);
+			}
+			for (const [index, argumentGroup] of this.args.entries()) {
+				this.currentRun = 0;
+				while (this.currentRun < this.runs) {
+					let result = undefined;
+					try {
+						result = this.candidate.apply(undefined, argumentGroup);
+						if (this.asynchronous) {
+							result = await Promise.resolve(result);
+						}
+					} catch (error) {
+						console.warn('MicroTestRunner: Test failed.', error);
+					}
+					const condition = this.conditions[index];
+					if (typeof condition === 'function') {
+						this.passing.push(condition(result));
+					} else {
+						this.passing.push(result === this.conditions[index]);
+					}
+					this.currentRun++;
+				}
+			}
+
+			// Return false if any one of the tests failed.
+			resolve(!this.passing.includes(false));
+		});
 
 		// Return false if any one of the tests failed.
-		return !this.passing.includes(false);
+		return (this.asynchronous ? promise : !this.passing.includes(false)) as Async extends true ? Promise<boolean> : boolean;
 	}
 }
 
