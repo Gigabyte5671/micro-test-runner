8b101fb43fef648db92049269cd9b15b44185dea
Giga <gigabyte5671@gmail.com>
2024-10-22 09:57:13 +1300

    Create failure test
    To validate that failure logging is formatted correctly.

---
+++ tests/failure.test.ts
@@ -0,0 +1,24 @@
+import test, { Severity } from '../src/index.js';
+
+export default function failureTest (): void {
+	function candidate (): string {
+		try {
+			test((a: number, b: number) => a + b)
+				.logging('Failure Candidate', Severity.ERROR, undefined, true)
+				.times(3)
+				.with(196.5, 42)
+				.with(10, undefined)
+				.with('Hello', ' world')
+				.expect(238.5, Number.isNaN, 9);
+
+			return '';
+		} catch (error) {
+			return String(error);
+		}
+	}
+
+	test(candidate)
+		.logging('Failure', Severity.ERROR, undefined, true)
+		.times(3)
+		.expect((error: string) => error.startsWith('Error: ✕ Failure Candidate test failed.\nExpected: 9\nReceived: Hello world'));
+}
--- tests/index.ts
+++ tests/index.ts
@@ -1,9 +1,11 @@
 import asyncTest from './async.test.js';
 import contextTest from './context.test.js';
+import failureTest from './failure.test.js';
 import integrationTest from './integration.test.js';
 import performanceTest from './performance.test.js';
 
 asyncTest();
 contextTest();
+failureTest();
 integrationTest();
 performanceTest();
